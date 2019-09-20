b7b98a3f98db7e5b20fe9469c21bed46120d4ce4
Jagdeepkhaira <jagdeepkhaira559@gmail.com>
2019-09-20 08:34:15 -0400

    Added the new file changes findsetidfiles.sh

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -10,9 +10,12 @@
 # The listing should include the file name, owner, and size in MBytes and be displayed after the listings of setuid and setgid files
 
 echo "Setuid files:"
-echo "============="
+echo "============================"
+# Task 1 - to find and display the setgid files
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 
-
-echo "============="
+# Task 2 - to sort the size of file, show in format
+echo "+++++++++++++++++++++++++++"
 find / 2>/dev/null -type f -exec ls -l --block-size=M {} + | sort -hr -k 5 | head -n 10 | awk '{print $5, $3, $9}'
+
+exit
