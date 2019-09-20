0c1edbb09a8d63b0b35ec34068ecdeab9e09abdf
Jagdeepkhaira <jagdeepkhaira559@gmail.com>
2019-09-20 09:52:16 -0400

    Final changes the findsetidfiles.sh

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -9,16 +9,19 @@
 # Task 2 for the exercise is to modify it to also display the 10 largest files in the system, sorted by their sizes
 # The listing should include the file name, owner, and size in MBytes and be displayed after the listings of setuid and setgid files
 
+# User Files
 echo "setuid files:"
-echo "**************** Step 1 ***************************"
+echo "**************** STEP 1 - SETUID FILES ***************************"
 find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 
+# Group id files
 # Task 1 - to find and display the setgid files
-echo "=========== LIST OF FORMATTED FILES ================="
+echo "setgid files:"
+echo "================= LIST OF FORMATTED FILES ========================"
 find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 
 # Task 2 - to sort the size of file, show in format
-echo "+++++++++++ 10 LARGEST FILES SORTED ++++++++++++++++"
+echo "+++++++++++++++++ 10 LARGEST FILES SORTED ++++++++++++++++++++++++"
 find / 2>/dev/null -type f -exec ls -l --block-size=M {} + | sort -hr -k 5 | head -n 10 | awk '{print $5, $3, $9}'
 
 exit
