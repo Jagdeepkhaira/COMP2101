3ebbc4996888ebc1dee59a87c86245ce920ce8aa
Jagdeepkhaira <jagdeepkhaira559@gmail.com>
2019-09-20 09:06:19 -0400

    Changed the findsetidfiles.sh

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -17,6 +17,6 @@ find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 
 # Task 2 - to sort the size of file, show in format
 echo "+++++++++++ 10 LARGEST FILES SORTED ++++++++++++++++"
-find / -type f -exec ls -l --block-size=M {} + | sort -hr -k 5 | head -n 10 | awk '{print $5, $3, $9}'
+find / 2>/dev/null -type f -exec ls -l --block-size=M {} + | sort -hr -k 5 | head -n 10 | awk '{print $5, $3, $9}'
 
 exit
