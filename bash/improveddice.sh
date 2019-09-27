e26d40d922d7bfdb7eebb4a348c0f7c593b88820
Jagdeepkhaira <jagdeepkhaira559@gmail.com>
2019-09-27 10:03:15 -0400

    Added the new One improveddice.sh

--- bash/improveddice.sh
+++ bash/improveddice.sh
@@ -5,21 +5,29 @@
 
 # Improve this script by re-organizing it to:
 #  put the number of sides in a variable which is used as the range for the random number
+
 #  put the bias, or offset, for the RANDOM number in a variable (it is 1 for our purposes)
 #     - you can think of this as the minimum value for the generated number
 #  roll the dice using the variables for the range and bias i.e. RANDOM % range + bias
 #  generate the sum of the dice
 #  generate the average of the dice
 #  display a summary of what was rolled, and what the result was
-
 # Tell the user we have started processing
-echo "Rolling..."
+
+# IMPROVED script...................
+sides=6
+bias=1
+
+echo "         .Rolling the dice.
+......    ......    ......     ......."
 # roll the dice and save the results
-die1=$(( RANDOM % 6 + 1))
-die2=$(( RANDOM % 6 + 1 ))
+dice1=$(( RANDOM % sides + bias ))
+dice2=$(( RANDOM % sides + bias ))
 # sum up the rolls
-sum=$(( die1 + die2 ))
+sum=$(( dice1 + dice2 ))
 # average
-avg=$(($sum / 2))
+average=$(($sum / 2))
 # display the results
-echo "Rolled $die1, $die2 for a $sum and average $avg"
+echo "Rolled Outcomes $dice1, $dice2 "
+echo "Sum of rolled Dices $dice1, $dice2 for a $sum"
+echo "Average of the rolled dices  $average"
